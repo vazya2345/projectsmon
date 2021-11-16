1c4131a99a4542d2c433347640596e273d5290ef
aziz <aziz@ubuntu2004.mshome.net>
2021-11-16 13:10:28 +0000

    db.example file added

---
+++ config/db_example.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=your_dbname',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
